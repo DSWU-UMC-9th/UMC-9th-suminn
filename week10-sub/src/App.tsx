8a32e95083ecb35ce0d63bbfdd651cfde3dc7176
sumin <sumin990103@naver.com>
2025-12-15 00:58:48 +0900

    Week10 sub mission1 미션 제출

---
+++ week10-sub/src/App.tsx
@@ -0,0 +1,12 @@
+import UseCallbackPage from "./UseCallbackPage";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex flex-col justify-center items-center h-dvh">
+    <UseCallbackPage />
+    </main>
+  );
+}
+
+export default App;
---
+++ week10-sub/src/components/TextInput.tsx
@@ -0,0 +1,18 @@
+import {memo} from 'react';
+
+interface ITextInput {
+    onChange: (text: string) => void;
+}
+
+const TextInput = ({onChange}: ITextInput) => {
+    return (
+        <input
+        type='text'
+        className='border p-4 rounded-lg'
+        onChange={(e) => onChange(e.target.value)}
+        />
+    );
+
+};
+
+export default memo(TextInput);
